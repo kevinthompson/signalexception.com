152918a2b2fb12f938def732ab3e2b42f7cf1b59
Kevin Thompson <kevin@kevinthompson.info>
2012-12-07 15:04:01 -0800

    Remove live reload.

--- config.rb
+++ config.rb
@@ -29,7 +29,6 @@ set :js_dir, 'javascripts'
 set :images_dir, 'images'
 set :markdown_engine, :redcarpet
 set :markdown, fenced_code_blocks: true, autolink: true
-activate :livereload
 
 # Compass
 compass_config do |config|
