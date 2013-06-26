d6871cb50ee22fa377258feb2c7292910363baa0
Kevin Thompson <kevin@kevinthompson.info>
2013-06-26 14:16:37 -0700

    Create article_title helper to avoid orphaned titles.

--- helpers/blog.rb
+++ helpers/blog.rb
@@ -3,3 +3,10 @@ def articles_by_month
     "#{Date::MONTHNAMES[article.date.month]} #{article.date.year}"
   end
 end
+
+def article_title(title)
+  title_parts = title.split(/\s/)
+  end_of_title = title_parts.pop(2).join('&nbsp;')
+  beginning_of_title = title_parts.join(' ')
+  "#{beginning_of_title} #{end_of_title}"
+end
